ab1b64cb759544e20a4bc8eedf428efb38183233
CharlyHdz13 <89402038+CharlyHdz13@users.noreply.github.com>
2022-01-08 15:44:42 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,19 @@ total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median = median(PS
 # Create summary for each production lot
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI),.groups = 'keep')
 
+# Create a sample from the suspension_coil
+suspension_coil_sample <- sample(suspension_coil$PSI,20)
+
+# t test to verify if there are no differences
+t.test(x = suspension_coil_sample,mu = total_summary$Mean)
+
+# Get the PSI values for each lot
+suspension_coil_lot1_PSI <- subset(suspension_coil,Manufacturing_Lot == "Lot1", PSI)
+suspension_coil_lot2_PSI <- subset(suspension_coil,Manufacturing_Lot == "Lot2", PSI)
+suspension_coil_lot3_PSI <- subset(suspension_coil,Manufacturing_Lot == "Lot3", PSI)
+
+# Perform a t test to each lot
+t.test(x = suspension_coil_lot1_PSI, mu = total_summary$Mean)
+t.test(x = suspension_coil_lot2_PSI, mu = total_summary$Mean)
+t.test(x = suspension_coil_lot3_PSI, mu = total_summary$Mean)
+
