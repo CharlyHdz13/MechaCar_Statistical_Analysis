1e074920de46b9e4adfc9643b7ed81db65f7ee80
CharlyHdz13 <89402038+CharlyHdz13@users.noreply.github.com>
2022-01-08 11:57:09 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,7 @@
 # Import libraries
 library(dplyr)
 
-# Import csv file as a dataframe
+# Import MechaCar_mpg.csv file as a dataframe
 mecha_car_mpg <- read.csv("Resources/MechaCar_mpg.csv")
 
 # create linear model
@@ -9,3 +9,13 @@ model <- lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+A
 
 # Extract p-value and r-squared value for the regression
 summary(model)
+
+# Import Suspension_Coil.csv file as a dataframe
+suspension_coil <- read.csv("Resources/Suspension_Coil.csv")
+
+# Create summary table for the PSI column
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI))
+
+# Create summary for each production lot
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI),.groups = 'keep')
+
